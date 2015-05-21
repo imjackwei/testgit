9e8c1b2324cf9dfcb43b78d32a438fc0d0900b8b
user <user@user-PC>
2015-05-21 14:38:32 +0800

    14:38 commit test

--- forGitTest/src/com/forGitTest/test1.java
+++ forGitTest/src/com/forGitTest/test1.java
@@ -4,7 +4,9 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");		
+		System.out.println("test");	
+		
+		System.out.println("test");
 
 	}
 	public int add(int a, int b){
